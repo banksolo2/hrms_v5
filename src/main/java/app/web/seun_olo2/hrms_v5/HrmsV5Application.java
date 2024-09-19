888b91709fd599fc163c2a894afbcf74e28f2585
banksolo2 <seunolo2@gmail.com>
2024-09-19 16:25:34 +0100

    First push
    First push

---
+++ src/main/java/app/web/seun_olo2/hrms_v5/HrmsV5Application.java
@@ -0,0 +1,13 @@
+package app.web.seun_olo2.hrms_v5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsV5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmsV5Application.class, args);
+	}
+
+}
---
+++ src/test/java/app/web/seun_olo2/hrms_v5/HrmsV5ApplicationTests.java
@@ -0,0 +1,13 @@
+package app.web.seun_olo2.hrms_v5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmsV5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
